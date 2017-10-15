168c6cbccc6b6faea45dffcfee866c6218e9232f
Daniel Ghazvini <31317901+SirOxorsid@users.noreply.github.com>
2017-10-15 13:43:31 -0500

    Added methods to Weather.java

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the main Test file!");
+		
+		// Testing out with Fahrenheit
+		Weather weatherObj = new Weather(96.4);
+		
+		System.out.println(weatherObj.getTemp());
 
 	}
 
