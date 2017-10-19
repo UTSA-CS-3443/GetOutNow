b1513c1bc19967d9020fb963d3d1dcf73afad997
cbumanglag15 <c.bumanglag.15@gmail.com>
2017-10-19 11:49:27 -0500

    Test: SettingsUI.java addition
    This is just a test to see if the file is pushed to Github

---
+++ src/SettingsUI.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package settings;
+
+/**
+ * @author cbuma
+ *
+ */
+public class SettingsUI {
+
+	/**
+	 * 
+	 */
+	public SettingsUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
