f0633c2c3049d433951e6607fec6a28af3b21a8a
Clarence Bumanglag <c.bumanglag.15@gmail.com>
2017-10-26 12:56:43 -0500

    Created a Settings.java file. Barebones class at the moment.

---
+++ src/settings/Settings.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package settings;
+
+/**
+ * Settings.java is a section of the main GUI that navigates the user into
+ * changing the application's settings. It will call other java classes that
+ * will change seperate parts of the GUI.
+ * 
+ * @author Clarence Bumanglag / pao952
+ *
+ */
+public abstract class Settings {
+
+	// <<<constructor>>>
+	public Settings (/* Planning on fixing params*/) {
+		/*
+		 * Insert variables here. This should be a abstract class.
+		 */
+	}
+}
