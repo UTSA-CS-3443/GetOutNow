d8c070460d00544843dc7dc07f9c3c8904c3f209
SirOxorsid <danielghazvini@gmail.com>
2017-11-13 13:49:40 -0600

    Updated WeatherScrape.java to now grab San Antonio weather information.

--- src/data/Test.java
+++ src/data/Test.java
@@ -1,28 +1,22 @@
 package data;
-import java.util.Scanner;
+import java.io.IOException;
 
 /**
  * Main file for testing.
  * @author Daniel Ghazvini dib952
- *
  */
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		System.out.println("This is the main Test file!");
-		
+	public static void main(String[] args) throws IOException {
+				
 		// Testing out with Fahrenheit
 		Weather weatherObj = new Weather(96.4);
 		System.out.println(weatherObj.getTemp());
 		
+		// Prints out the current weather in San Antonio.
+		System.out.println(WeatherScrape.Valhalla());
 		
-/*		System.out.println("Enter your City:");
-		Scanner scanner = new Scanner(System.in);
-		String cityName = scanner.nextLine();
-		System.out.println("Your city is " + cityName);
-		*/
 		
 	}
 
--- src/data/WeatherScrape.java
+++ src/data/WeatherScrape.java
@@ -1,7 +1,11 @@
 package data;
 
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 /**
  * Class to scrape weather data from the web.
@@ -9,12 +13,28 @@ import org.jsoup.nodes.*;
  *
  */
 public class WeatherScrape {
+		
+/**
+ * Class to retrieve weather information from webpage.
+ * @return String
+ * @throws IOException
+ */
+	public static String Valhalla() throws IOException {
+		
+		// Webpage used to gather weather information in San Antonio.
+		Document doc = Jsoup.connect("http://forecast.weather.gov/MapClick.php?lat=29.46&lon=-98.5/").get();
+		Elements results = doc.getElementsByClass("myforecast-current-lrg");
+		
+		// Store class information with up-to-date weather in htmlString.
+		String htmlString = results.toString();
+		
+		// Used to pull only the degrees in Fahrenheit.
+		Pattern pattern = Pattern.compile(">(.*)<");
+		Matcher matcher = pattern.matcher(htmlString);
+		if (matcher.find())
+		    htmlString = matcher.group(1);
 	
-	private int WeatherFahrenheit;
-	
-	// Webpage used to gather weather information (San Antonio).
-//	Document doc = Jsoup.connect("http://forecast.weather.gov/MapClick.php?lat=29.46&lon=-98.5/").get();
-
-	
-
+		return htmlString;
+		
+	}
 }
