19c4ec004357388c775a39311e2de4186f7d9dea
Ben Bravo <benbravo1596@live.com>
2017-11-21 17:56:00 -0600

    Added toString function and JavaDoc to Event

--- src/profile/Event.java
+++ src/profile/Event.java
@@ -11,30 +11,58 @@ public class Event {
 	private String date;
 	private String eventName;
 	
-	
-	public Event(String event, String date) {
+	/**
+	 * Constructor for Event 
+	 * @param String: date
+	 * @param String: event
+	 */
+	public Event(String date, String event) {
 		this.setEventName(event);
 		this.setDate(date);
 	}
-
-
+	
+	/**
+	 * 
+	 * @return String: date
+	 */
 	public String getDate() {
 		return date;
 	}
-
-
+	
+	/**
+	 * Sets date
+	 * @param String: date
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
 
-
+	/**
+	 * getter for eventName
+	 * @return Sring:eventName
+	 */
 	public String getEventName() {
 		return eventName;
 	}
 
-
+	/**
+	 * Sets eventName
+	 * @param String Event
+	 */
 	public void setEventName(String eventName) {
 		this.eventName = eventName;
 	}
 	
+	/**
+	 * To String Function
+	 * @param Event e
+	 * @return String 
+	 */
+	public String toString(Event e) {
+		return this.date + " " + this.eventName;
+	}
 }
+
+
+
+
