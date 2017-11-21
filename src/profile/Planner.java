d8669c4399c219f3c5b3d7e4e903083d618b153e
whoismissing <whereismissing@gmail.com>
2017-11-21 17:59:43 -0600

    Planner and TestPlanner adjusted to use Event.java

--- src/profile/Planner.java
+++ src/profile/Planner.java
@@ -1,9 +1,7 @@
 package profile;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,36 +30,36 @@ import java.util.Map;
  */
 public class Planner {
 	private String username;
-	private Map<String, ArrayList<String>> userPlan;
-	private ArrayList<String> eventList; // arrayList of events (sorted by descending order by date)
+	//private Map<String, ArrayList<String>> userPlan;
+	private ArrayList<Event> eventList; // arrayList of events (sorted by descending order by date)
 
 	// Declare variables for File I/O
 	private static final String PLANNERFILE = "planner.txt";
-	private static BufferedReader in;
+	//private static BufferedReader in;
 	private static BufferedWriter out;
-	private static FileReader filein;
+	//private static FileReader filein;
 	private static FileWriter fileout;
-	private static String line;
+	//private static String line;
   
 	// constructor
 	public Planner(String username) {
 		this.username = username;
-		this.eventList = new ArrayList<String>();
-		this.userPlan = new HashMap();
+		this.eventList = new ArrayList<Event>();
+		//this.userPlan = new HashMap();
 	}
   
 	// add an event to arrayList
-	public void scheduleEvent(String event) {
+	public void scheduleEvent(Event event) {
 		this.eventList.add(event);
 	}
   
 	// remove event from arrayList
-	public void removeEvent(int index) {
-		this.eventList.remove(index);
+	public void removeEvent(Event event) {
+		this.eventList.remove(event);
 	}
 	
 	// return ArrayList of eventList
-	public ArrayList<String> getEventList() {
+	public ArrayList<Event> getEventList() {
 		return this.eventList;
 	}
 	
@@ -75,8 +73,8 @@ public class Planner {
 			Planner.out = new BufferedWriter(Planner.fileout);
 			Planner.out.write(this.username + "\n");   // write username to file
 			// write list of events to file
-			for (String str : this.eventList) {
-				Planner.out.write(str + "\n");
+			for (Event event : this.eventList) {
+				Planner.out.write(event + "\n");
 			}
 			// close FILE IO
 			Planner.out.close();
