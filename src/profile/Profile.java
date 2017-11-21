015bf5e64be2782b029886b853b9025b12a405fe
Clarence <c.bumanglag.15@gmail.com>
2017-11-20 20:55:15 -0600

    Finished Javadocs for Profile.java

--- src/profile/Profile.java
+++ src/profile/Profile.java
@@ -26,12 +26,12 @@ import java.util.ArrayList;
  * for any changes via the Settings.java file.
  * 
  * @author Clarence Bumanglag
- * @author Christian San Juan
  *
  */
 
 public class Profile {
 
+	// Profile's variables
 	private String firstName;		//first name
     private String lastName;		//last name
     private String email;			//email
@@ -39,9 +39,34 @@ public class Profile {
     private String name;			//first name and last name combined
     private String password;		//password
 
-    /*
-     * Constructor
-     * Creates Profile object
+    /**
+     * <<<<<PRIMARY CONSTRUCTOR>>>>>
+     * 
+     * Constructs a Profile object using the information
+     * provided by an ArrayList. The ArrayList gets its information
+     * from the data file "profiledata.txt"
+     * 
+     * @param An ArrayList holding user information
+     */
+    public Profile (ArrayList<String> userInfo) {
+    	this.username = userInfo.get(0);
+    	this.email = userInfo.get(1);
+    	this.password = userInfo.get(2);
+    	this.name = userInfo.get(3);
+    }
+    
+    /**
+     *      * <<<<<SECONDARY CONSTRUCTOR>>>>>
+     * Creates Profile object. This constructor is only accessed
+     * when a the method createProfile() is called from Account.java
+     * (Account.createProfile()). Should lead to creating a new profile
+     * and saving the new Profile contents into the data file "profiledata.txt"
+     * 
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param username
+     * @param password
      */
     public Profile(String firstName, String lastName, String email, String username, String password) {
     	
@@ -52,62 +77,103 @@ public class Profile {
         this.name = this.firstName + " " + this.lastName;
         this.password = password;
     }
-    
-    /*
-     * Secondary Constructor
-     */
-    public Profile (ArrayList<String> userInfo) {
-    	this.username = userInfo.get(0);
-    	this.email = userInfo.get(1);
-    	this.password = userInfo.get(2);
-    	this.name = userInfo.get(3);
-    }
-    
 
+    /**
+     * Getter for this Profile's last name
+     * @return last name
+     */
     public String getLastName() {
 		return lastName;
 	}
 
+    /**
+     * Setter for this Profile's last name
+     * @param lastName
+     */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * Getter for this Profile's email
+	 * 
+	 * @return email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Setter for this Profile's email
+	 * @param email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * Getter for this Profile's username
+	 * @return
+	 */
 	public String getUsername() {
 		return username;
 	}
-
+	
+	/**
+	 * Setter for this Profile's username
+	 * @param username
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * Getter for this Profile's Name
+	 * 
+	 * @return full name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Setter for this Profile's name
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
     
+	/**
+	 * Getter for this Profile's password
+	 * 
+	 * @return password
+	 */
     public String getPassword() {
     	return this.password;
     }
     
+    /**
+     * Setter for this Profile's password
+     * 
+     * @param password
+     */
     public void setPassword(String password) {
     	this.password = password;
     }
     
+    /**
+     * Getter for this Profile's first name
+     * @return first name
+     */
     public String getFirstName() {
 		return firstName;
 	}
 
+    /**
+     * Setter for this Profile's first name
+     * @param firstName
+     */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
