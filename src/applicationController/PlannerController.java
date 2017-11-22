061ce844121b5a8e83e5b69cfd2c6a576cab631e
whoismissing <whereismissing@gmail.com>
2017-11-21 19:56:32 -0600

    Initialized PlannerController.java

--- src/applicationController/PlannerController.java
+++ src/applicationController/PlannerController.java
@@ -5,21 +5,48 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 
 /**
  * 
  * @author Julian Chan / xmp183
+ * PlannerController.java is the controller for Planner.fxml
+ * It will allow users to add Dates and Events to a Planner via Planner.java
  *
  */
 public class PlannerController implements Initializable {
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+	@FXML
+	private FXMLLoader plannerLoader;
+	
+	@FXML
+	private Parent plannerParent;
+	
+	/**
+	 * CONSTRUCTOR
+	 */
+	public PlannerController() {
 		
 	}
 	
+	/**
+	 * This method initializes FXML variables to be used
+	 * and sets the values of the variables accordingly.
+	 */
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		plannerLoader = new FXMLLoader(getClass().getResource("/applicationView/Planner.fxml"));
+		plannerParent = null;
+
+	}
+	
+	/**
+	 * Event Handler
+	 * @param event
+	 */
 	@FXML
 	public void handleButtonAction(ActionEvent event) {
 		
